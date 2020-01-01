761581d9f02f9b683cf3230ebc1be72440b99fce
صفوان القليطي <safwanalqulayti@gmail.com>
2020-01-01 22:36:16 +0300

    solutions

---
+++ js/arena.js
@@ -0,0 +1,17 @@
+class Arena extends Gladiator{
+
+    constructor(name){
+        this.name =name
+        this.gladiators = []
+        this.cap  = function(){
+
+            this.name.charAt(0).toUpperCase()
+        }
+        this.cap()
+    }
+
+    addGladiator(_Gladiator){
+
+        this.gladiators.push()
+    }
+}
