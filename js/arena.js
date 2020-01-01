e3cd9c9d02562f75e1c68acaff3cdf88642adeb1
صفوان القليطي <safwanalqulayti@gmail.com>
2020-01-01 22:54:16 +0300

    solutions modified

--- js/arena.js
+++ js/arena.js
@@ -1,17 +1,76 @@
+
 class Arena extends Gladiator{
 
-    constructor(name){
-        this.name =name
+    constructor(arenaName,name,weapon){
+        super(name, weapon)
+        this.arenaName =arenaName
         this.gladiators = []
         this.cap  = function(){
 
-            this.name.charAt(0).toUpperCase()
+            this.arenaName.charAt(0).toUpperCase()
         }
         this.cap()
     }
 
-    addGladiator(_Gladiator){
+ fight(){
 
-        this.gladiators.push()
+  
+  
+
+    this.gladiators.map(x=>{
+     
+      if (x.weapon == "Spear" || x.weapon =="Trident"){
+        this.gladiators.splice(x.weapon.includes("Trident")) ;
+         
+      }
+      else if (x.weapon == "Spear" || x.weapon =="Club"){
+        this.gladiators.splice(x.weapon.includes("Spear")) ;
+         
+      }
+
+        
+   else if (x.weapon == "Club" || x.weapon =="Trident"){
+        this.gladiators.splice(x.weapon.includes("Club")) ;
+         
+      }
+      else{
     }
+    
+    
+        
+     })
+ }
+
+   
+    addGladiator(Gladiator){
+
+       
+        if (this.gladiators.length == 2){
+          console.log("the arena is full call fight before adding new Gladiator:")
+           
+         }else{
+ this.gladiators.push(Gladiator)
+         }
+    }
+
+
+    gladiatorsArr(){
+        console.log(this.gladiators)
+
+    }
+
+   
 }
+
+c = new Arena("coloisum")
+
+c.addGladiator(safwan)
+c.addGladiator(ali)
+c.gladiatorsArr()
+
+ c.addGladiator(ali)
+
+c.fight()
+ 
+ 
+ c.gladiatorsArr()
