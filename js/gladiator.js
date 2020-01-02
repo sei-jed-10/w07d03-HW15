1aa7abd451c1c16eb2bce0d4fddbc864cc00bedf
Mohammedalamry <Mohammedalamry@users.noreply.github.com>
2020-01-02 08:28:45 +0300

    hw15 not complete

---
+++ js/gladiator.js
@@ -0,0 +1,22 @@
+class Gladiator {   
+constructor(name){                    
+     weapon (one of Spear, Club, Trident)
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+} 
+
+const max = new Gladiator("Maximus", "Trident");
+console.log(max.name);  
+console.log(max.weapon);   
