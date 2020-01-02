7578b4c48b38df697e535b2836acd8e978cde8cf
Abdullah Binmahfouz <abinm670@mtroyal.ca>
2020-01-02 09:15:00 +0300

    Bonus question is missing

---
+++ js/gladiator.js
@@ -0,0 +1,15 @@
+class Gladiator {
+
+    constructor(name, weapon) {
+        this.name = name;
+        this.weapon = weapon;
+        //["Spear", "Club", "Trident"];
+    }
+
+
+}
+
+let max = new Gladiator("abdullah", "Club");
+
+
+console.log(max.weapon); 
