19bf2959f3abc6c2124a01e4af2a66a808b486a4
wardahmad <whitelight43@hotmail.com>
2020-01-04 23:36:05 +0300

    finishedHW15

--- js/arena.js
+++ js/arena.js
@@ -1,59 +1,67 @@
-class Gladiator{
-    constructor(name,weapon) {
-        this._name = name;
-        this._weapon = weapon;
-      } 
-
-    set addGladiator(name){
-        this._name = name;
-        //this._weapon = weapon;
-
-      }
-}
-
-
-class Arena extends Gladiator{
+class Arena{
     constructor(name) {
-        this._name = name;
-       }
-
-    get name() {
-        return this._name.charAt(0).toUpperCase() + this._name.substring(1);;
-      }
-
+        this.name = name.charAt(0).toUpperCase() + name.substring(1);
+        this.gladiators = [];
+        this.gladiatorsName = [];
+        this.counter = 1;
+    }
     addGladiator(name){
-        super (name)
-        //this._gladiators.push(gladiator)
-        //console.log(this._gladiators)
+        if (this.gladiators.length <= 1){
+            this.gladiators.push("Gladiator"+this.counter);
+            this.gladiatorsName.push(name);
+            this.counter++;
+        }
+    }
+    fight(){
+        //console.log(this.gladiatorsName[0].name)
+        //console.log(this.gladiatorsName[0].weapon)
+        //console.log(this.gladiatorsName[1].name)
+        //console.log(this.gladiatorsName[1].weapon)
 
-      }
+        var glaOneW = this.gladiatorsName[0].weapon;
+        var glaTowW = this.gladiatorsName[1].weapon;
+        // console.log(glaOneW)
+        // console.log(glaTowW)
+
+        // Trident beats Spear
+        // Spear beats Club
+        // Club beats Trident
+        // If the two gladiators have the same weapon, they are both eliminated.
+
+        if (this.gladiatorsName.length == 2){
+            if (glaOneW == "Trident" && glaTowW == "Spear"){
+                this.gladiators=[this.gladiatorsName[0].name]
+            }
+            else if (glaOneW == "Spear" && glaTowW == "Trident"){
+                 this.gladiators=[this.gladiatorsName[1].name]
+            }
+            else if (glaOneW == "Spear" && glaTowW == "Club"){
+                 this.gladiators=[this.gladiatorsName[0].name]
+            }
+            else if (glaOneW == "Club" && glaTowW == "Spear" ){
+                 this.gladiators=[this.gladiatorsName[1].name]
+            }
+            else if (glaOneW == "Club" && glaTowW == "Trident"){
+                 this.gladiators=[this.gladiatorsName[0].name]
+            }
+            else if (glaOneW == "Trident" && glaTowW == "Club"){
+                 this.gladiators=[this.gladiatorsName[1].name]
+            }
+            else if (glaOneW == glaTowW){
+                 console.log("eliminated")
+            }
+        }else{
+            console.log("gladiatorsName.length < 2")
+        }
+    }
 }
+const colosseum = new Arena("Colosseum");
 
-// class Arena extends Gladiator{
-//     constructor(name) {
-//         this._name = name;
-//         this._gladiators = [];
-//       }
+colosseum.addGladiator(max);
+colosseum.addGladiator(titus);
+colosseum.addGladiator(andronicus);
+console.log(colosseum.gladiators.length); // => 2
+console.log(colosseum.gladiators);
+colosseum.fight();
 
-//       get name() {
-//         return this._name.charAt(0).toUpperCase() + this._name.substring(1);;
-//       }
-
-//       addGladiator(gladiator){
-//         this._gladiators.push(gladiator)
-//         console.log(this._gladiators)
-
-//       }
-
-//     //   gladiators(gladiator){
-//     //       this._gladiators.push(gladiator)
-//     //       console.log(this._gladiators)
-
-
-//     //   }
-
-// }
-
-const colosseum = new Arena("megalopolis");
-console.log(colosseum.name); // => Colosseum
-//colosseum.gladiators("ward")
+console.log(colosseum.gladiators); // => [max]
--- js/gladiator.js
+++ js/gladiator.js
@@ -1,11 +1,11 @@
 class Gladiator{
-    constructor(name,weapon) {
-        this.name = name;
-        this.weapon = weapon;
-      }
-      
-}
+    constructor(name,weapon){
+      this.name = name;
+      this.weapon = weapon;
+    }
+  }
+//weapon (one of Spear, Club, Trident)
 
-const max = new Gladiator("Maximus", "Trident");
-console.log(max.name); // "Maximus"
-console.log(max.weapon); // "Trident"
+const max = new Gladiator("Maximus","Trident");
+const titus = new Gladiator("Titus","Spear");
+const andronicus = new Gladiator("Andronicus","Club");
